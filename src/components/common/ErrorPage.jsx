9dc02b1ee25473ae9f90cda801ed553f729140d9
amr <amrkhaled286@gmail.com>
2025-09-29 20:00:05 +0300

    edit error page

--- src/components/common/ErrorPage.jsx
+++ src/components/common/ErrorPage.jsx
@@ -1,7 +1,14 @@
-export default function ErrorPage() {
+import React from 'react';
+
+function ErrorPage() {
   return (
     <div className="flex min-h-screen items-center justify-center p-4">
-      <img src="./404 error lost in space.gif" alt="" />
+      <img
+        src="./404_error_lost_in_space-removebg-preview.webpf"
+        loading="lazy"
+        alt="Error page"
+      />
     </div>
   );
 }
+export default React.memo(ErrorPage);
